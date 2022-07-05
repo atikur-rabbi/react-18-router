ad42e47eaedc0541aa22b5674092ffd7ecd41621
atikur-rabbi <msawon94@gmail.com>
2022-07-05 09:55:26 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-18-router
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-18-router)
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import './style.css';
+import About from './components/About';
+import Home from './components/Home';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <p>Start editing to see some magic happen :)</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
